efdff6e1530c5c397c7838eee79f2e8f709d585e
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-10-23 00:01:01 +0530

    add

---
+++ findTheoutput.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    // int i;
+    // for( i=0; i<=5; i++);
+    //     printf("%d", i);
+    // return 0;
+    // char name[] =  "ashsish";
+    // printf("%s", name);
+    int ashish[2];
+    ashish[3] = 4;
+   
+}
