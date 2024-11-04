60e9652ae57554023210875c578146e897ad0488
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-11-04 11:00:51 +0530

    added a optimal solution

--- Array/q2.c
+++ Array/q2.c
@@ -1,35 +1,54 @@
 // find the second largest elem ;
 #include <stdio.h>
-// common approach not brute but bit better approach  
-int secondLargest(int arr[], int n){
- int maxelem = arr[0];
-    for(int i = 0  ; i<n ; i++){
-        if (arr[i]> maxelem)
+
+int secondLargest(int arr[], int n)
+{
+    // common approach not brute but bit better approach
+    //  int maxelem = arr[0];
+    //     for(int i = 0  ; i<n ; i++){
+    //         if (arr[i]> maxelem)
+    //         {
+    //             maxelem = arr[i];
+    //         }
+
+    //     }
+    //      int secondlarge = arr[0];
+    //     for(int i = 0 ; i<n; i++){
+
+    //         if(arr[i]>secondlarge && arr[i]!= maxelem){
+    //             secondlarge = arr[i];
+
+    //         }
+    //     }
+    //     return secondlarge ;
+    // optimal approach to find second largest elem
+    int largestelem = arr[0];
+    int smallestelem = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largestelem)
         {
-            maxelem = arr[i];
+            smallestelem = largestelem;
+            largestelem = arr[i];
         }
-        
-    }
-     int secondlarge = arr[0]; 
-    for(int i = 0 ; i<n; i++){
-       
-        if(arr[i]>secondlarge && arr[i]!= maxelem){
-            secondlarge = arr[i];
-            
+        else if(arr[i]<largestelem && arr[i]>smallestelem){
+            smallestelem = arr[i] ;
         }
     }
-    return secondlarge ;
+    return smallestelem ; 
 }
 
-int main(){
-     int n;
+int main()
+{
+    int n;
     printf("Enter the length of arr\n");
     scanf("%d", &n);
-    int arr[n]; 
-    for (int i = 0 ; i<n ; i++){
-        scanf("%d" , &arr[i]);
-    } 
-    int secondlarge = secondLargest(arr , n );
-    printf("second large elem %d" , secondlarge);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int secondlarge = secondLargest(arr, n);
+    printf("second large elem %d", secondlarge);
     return 0;
 }
