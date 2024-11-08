2de450f499ffb2ca574e892e74bde237edff99a0
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-11-08 11:12:52 +0530

    add

---
+++ binarysearch/q3.c
@@ -0,0 +1,16 @@
+// insert a elem 
+// int n = nums.size();
+//         int low = 0 ;
+//         int high = n-1;
+//         while(high>=low){
+//             int mid = (low + high )/2;
+//             if(nums[mid]==target ) return mid ;
+//             else if(nums[mid]>=target){
+//                 high = mid-1 ;
+
+//             }
+//             else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return low ;
